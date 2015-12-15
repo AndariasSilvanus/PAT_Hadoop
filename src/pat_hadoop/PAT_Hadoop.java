6fa48adf26d8ed13e0886055f9c693f0c18bd41c
Andarias <asilvanus5@gmail.com>
2015-12-15 12:06:03 +0700

    first commit

---
+++ src/pat_hadoop/PAT_Hadoop.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pat_hadoop;
+
+/**
+ *
+ * @author Andarias Silvanus
+ */
+public class PAT_Hadoop {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
